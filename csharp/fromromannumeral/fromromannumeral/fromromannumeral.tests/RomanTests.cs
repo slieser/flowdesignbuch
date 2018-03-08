2ff72e1a130e6a415ae8042a3bda055298692f9b
slieser <stefan@lieser-online.de>
2018-03-08 11:11:08 +0100

    Kata From Roman Numeral added

---
+++ csharp/fromromannumeral/fromromannumeral/fromromannumeral.tests/RomanTests.cs
@@ -0,0 +1,40 @@
+﻿using NUnit.Framework;
+
+namespace fromromannumeral.tests
+{
+    [TestFixture]
+    public class RomanTests
+    {
+        [Test]
+        public void Ziffern() {
+            Assert.That(Roman.FromRomanNumeral("I"), Is.EqualTo(1));
+            Assert.That(Roman.FromRomanNumeral("V"), Is.EqualTo(5));
+            Assert.That(Roman.FromRomanNumeral("X"), Is.EqualTo(10));
+            Assert.That(Roman.FromRomanNumeral("L"), Is.EqualTo(50));
+            Assert.That(Roman.FromRomanNumeral("C"), Is.EqualTo(100));
+            Assert.That(Roman.FromRomanNumeral("D"), Is.EqualTo(500));
+            Assert.That(Roman.FromRomanNumeral("M"), Is.EqualTo(1000));
+        }
+
+        [Test]
+        public void Addition() {
+            Assert.That(Roman.FromRomanNumeral("II"), Is.EqualTo(2));
+            Assert.That(Roman.FromRomanNumeral("VII"), Is.EqualTo(7));
+            Assert.That(Roman.FromRomanNumeral("XV"), Is.EqualTo(15));
+        }
+
+        [Test]
+        public void Subtraktion() {
+            Assert.That(Roman.FromRomanNumeral("IV"), Is.EqualTo(4));
+            Assert.That(Roman.FromRomanNumeral("IX"), Is.EqualTo(9));
+            Assert.That(Roman.FromRomanNumeral("CM"), Is.EqualTo(900));
+        }
+
+        [Test]
+        public void Integrationstest() {
+            Assert.That(Roman.FromRomanNumeral("XLII"), Is.EqualTo(42));
+            Assert.That(Roman.FromRomanNumeral("XCIX"), Is.EqualTo(99));
+            Assert.That(Roman.FromRomanNumeral("MMXIII"), Is.EqualTo(2013));
+        }
+    }
+}
---
+++ csharp/fromromannumeral/fromromannumeral/fromromannumeral/Roman.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace fromromannumeral
+{
+    public class Roman
+    {
+        private static readonly Dictionary<string, int> Digits = new Dictionary<string, int> {
+            { "I", 1 },
+            { "IV", 4 },
+            { "V", 5 },
+            { "IX", 9 },
+            { "X", 10 },
+            { "XL", 40 },
+            { "L", 50 },
+            { "XC", 90 },
+            { "C", 100 },
+            { "CD", 400 },
+            { "D", 500 },
+            { "CM", 900 },
+            { "M", 1000 }
+        };
+        
+        public static int FromRomanNumeral(string roman) {
+            var result = 0;
+
+            while (roman.Length > 0) {
+                var (value, remainder) = GetDigitFromRight(roman);
+                result += value;
+                roman = remainder;
+            }
+            
+            return result;
+        }
+
+        private static (int, string) GetDigitFromRight(string roman) {
+            foreach (var digit in Digits) {
+                if (roman.EndsWith(digit.Key)) {
+                    var value = digit.Value;
+                    var remainder = roman.Remove(roman.Length - digit.Key.Length);
+                    return (value, remainder);
+                }
+            }
+            throw new Exception($"No digit could be found in roman '{roman}'.");
+        }
+    }
+}
