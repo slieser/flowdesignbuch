06290df8368ae8175dcf532a3ff1cb0a5e62f318
slieser <stefan@lieser-online.de>
2018-03-08 14:57:53 +0100

    Test added

---
+++ java/loccount/tests/de/lieser_online/LocTests.java
@@ -0,0 +1,20 @@
+package de.lieser_online;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+public class LocTests {
+    @Test
+    public void LocStat_is_created_with_correct_values() {
+        List<String> lines = Arrays.asList("code", "   ", "   // comment");
+        LocStat result = Loc.CountLoc("xyz", lines);
+
+        assertEquals("xyz", result.Filename);
+        assertEquals(3, result.Total);
+        assertEquals(1, result.Loc);
+    }
+}
