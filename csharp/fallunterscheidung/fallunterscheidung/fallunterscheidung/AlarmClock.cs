8ac88c2fa7abf55cd448c5a2ff08fed0ba9c98f8
slieser <stefan@lieser-online.de>
2018-09-20 18:20:35 +0100

    Added some examples

---
+++ csharp/fallunterscheidung/fallunterscheidung/fallunterscheidung/AlarmClock.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace fallunterscheidung
+{
+    public class AlarmClock
+    {
+        public void WhenAlarmTimeArrives(TimeSpan remainingTime, Action onAlarmTime) {
+            if (remainingTime.TotalSeconds <= 0) {
+                onAlarmTime();
+            }
+        }
+
+        public bool AlarmTimeArrives(TimeSpan remainingTime) {
+            return remainingTime.TotalSeconds <= 0;
+        }
+        
+        public void Alarm() {
+            var remainingTime = GetRemainingTime();
+            WhenAlarmTimeArrives(remainingTime,
+                () => {
+                    Console.WriteLine("Alarm!");
+                });
+        }
+
+        public void Alarm2() {
+            var remainingTime = GetRemainingTime();
+            if (AlarmTimeArrives(remainingTime)) {
+                 Console.WriteLine("Alarm!");
+            }
+        }
+
+        private static TimeSpan GetRemainingTime() {
+            return new TimeSpan();
+        }
+    }
+}
