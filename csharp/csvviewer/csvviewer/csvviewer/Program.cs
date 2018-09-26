686594395a6fed95f703bd1f7cc6a1d9fa41edea
slieser <stefan@lieser-online.de>
2018-09-26 15:56:40 +0100

    csvviewer von Core auf Mono umgestellt.

--- csharp/csvviewer/csvviewer/csvviewer/Program.cs
+++ csharp/csvviewer/csvviewer/csvviewer/Program.cs
@@ -1,20 +1,15 @@
-﻿using System;
-using System.Text;
-
-namespace csvviewer
+﻿namespace csvviewer
 {
     public class Program
     {
         public static void Main(string[] args) {
-            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
-
             var interactors = new Interactors();
             var ui = new Ui();
 
-            Action start = () => {
+            void Start() {
                 var records = interactors.Start(args);
                 ui.Display(records);
-            };
+            }
 
             ui.MoveNext += () => {
                 var records = interactors.MoveNext();
@@ -25,7 +20,7 @@ namespace csvviewer
                 ui.Display(records);
             };
 
-            start();
+            Start();
             ui.Run();
         }
     }
