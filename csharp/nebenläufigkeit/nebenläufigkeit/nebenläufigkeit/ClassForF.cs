c63b2a8b87bf2a3d932be1dc13bd0e54a3546795
slieser <stefan@lieser-online.de>
2019-08-10 10:04:55 +0200

    ?

--- csharp/nebenläufigkeit/nebenläufigkeit/nebenläufigkeit/ClassForF.cs
+++ csharp/nebenläufigkeit/nebenläufigkeit/nebenläufigkeit/ClassForF.cs
@@ -5,29 +5,32 @@ namespace nebenläufigkeit
 {
     public class ClassForF
     {
-        public void f(int x, Action<int> onResult) {
-            Console.WriteLine("Calling A...");
-            A(x, onResult);
-            Console.WriteLine("A finished");
-        }
+public void f(int x, Action<int> onResult) {
+    Console.WriteLine("Calling A...");
+    A(x, onResult);
+    Console.WriteLine("A finished");
+}
 
-        private void A(int x, Action<int> onResult) {
-            var y = x + 42;
+private void A(int x, Action<int> onResult) {
+    var y = x + 42;
 
-            Console.WriteLine("Calling B on new thread...");
-            var thread = new Thread(() => {
-                var z = B(y);
-                onResult(z);
-            });
-            thread.Start();
-            Console.WriteLine("B finished");
-        }
+    Console.WriteLine("Calling B on new thread...");
+    var thread = new Thread(() => {
+        var z = B(y);
+        onResult(z);
+    });
+    thread.Start();
+    Console.WriteLine("B finished");
+}
 
-        private int B(int y) {
-            Console.WriteLine("B is working...");
-            Thread.Sleep(1000);  // Simulate long running calculation
-            Console.WriteLine("B did its work");
-            return y + 1;
-        }
-    }
+private int B(int y) {
+    Console.WriteLine("B is working...");
+
+    // Simulate long running calculation
+    Thread.Sleep(1000);  
+
+    Console.WriteLine("B did its work");
+    return y + 1;
+}
+}
 }
