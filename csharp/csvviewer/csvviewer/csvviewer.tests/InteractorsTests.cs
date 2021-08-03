995c511c7a798f80681b3a29f08912d25a14cbc4
Stefan Lieser <stefan@lieser-online.de>
2021-08-03 10:55:15 +0200

    Tests renamed

--- csharp/csvviewer/csvviewer/csvviewer.tests/InteractorsTests.cs
+++ csharp/csvviewer/csvviewer/csvviewer.tests/InteractorsTests.cs
@@ -16,22 +16,17 @@ namespace csvviewer.tests
         }
 
         [Test]
-        public void Start_mit_notwendigen_Parametern() {
+        public void Start_with_necessary_parameters() {
             var result = _sut.Start(new[] {_filename}).ToArray();
             Assert.That(result.Length, Is.EqualTo(11));
             Assert.That(result[0].Values, Is.EqualTo(new[]{"Name", "Age", "City"}));
         }
 
         [Test]
-        public void Start_mit_optionalem_PageLength_Parameter() {
+        public void Start_with_optional_PageLength_parameter() {
             var result = _sut.Start(new[] {_filename, "3"}).ToArray();
             Assert.That(result.Length, Is.EqualTo(4));
             Assert.That(result[0].Values, Is.EqualTo(new[]{"Name", "Age", "City"}));
-        }
-
-        [Test]
-        public void Start_WithOptionalPageLengthParameter_ReturnsRecordsForFirstPage() {
-            
         }
     }
 }
