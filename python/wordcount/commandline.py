ccad17922b01d10b7d3233b728b26fc936a79137
Stefan Lieser <stefan@lieser-online.de>
2021-08-03 10:50:42 +0200

    Read from file added

---
+++ python/wordcount/commandline.py
@@ -0,0 +1,5 @@
+def get_filename(argv):
+    if(len(argv) > 1):
+        return argv[1]
+    else:
+        return ""
---
+++ python/wordcount/file_provider.py
@@ -0,0 +1,3 @@
+def read_file(filename):
+    with open(filename, "r") as f:
+        return f.read()
