033cd346d8ca6237ba83968df85b06ac9f57bab4
Stefan Lieser <stefan@lieser-online.de>
2021-08-03 10:50:16 +0200

    .idea removed

--- python/wordcount/main.py
+++ python/wordcount/main.py
@@ -1,7 +1,15 @@
+import sys
+
+from commandline import get_filename
+from file_provider import read_file
 from interactors import start
 from ui import get_text, show_count
 
 if __name__ == '__main__':
-    text = get_text()
+    filename = get_filename(sys.argv)
+    if(filename == ""):
+        text = get_text()
+    else:
+        text = read_file(filename)
     count = start(text)
     show_count(count)
